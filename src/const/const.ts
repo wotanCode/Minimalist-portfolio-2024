1ac5de3f4b569eee36ee77e4819a94f078b794a1
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-09-25 17:46:28 -0300

    add githubUser constant

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,5 +1,7 @@
 export const PEDROYANEZ = "Pedro Yanez";
 
+export const GITHUBUSER = "pedroelhumano"
+
 export const ID = {
   //TODO: implementar correctamente Id's
   PEDROYANEZPHOTO: "pedro-yanez-photo",
