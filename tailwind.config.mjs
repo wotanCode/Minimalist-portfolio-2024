e2ce871c1441ee0f5a0d0ae6221eca823e0af3c7
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-10-09 17:57:17 -0300

    update theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,8 @@ export default {
     extend: {
       colors: {
         // Colores para tema oscuro https://colorhunt.co/palette/0306373c0753720455910a67
-        primary: "#910A67", // TODO
-        secondary: "#720455", // TODO
-        hagalRune: "#002441",
-        background: "#13151a",
-        backgroundWhite: "#eae5d4", // TODO: Mover a estilos de temas cuando exista.
+        primary: "rgba(var(--color-primary))",
+        secondary: "rgba(var(--color-secondary))",
       },
     },
   },
