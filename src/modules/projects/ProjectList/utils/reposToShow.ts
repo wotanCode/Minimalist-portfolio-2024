49946ae51952b7daceb4422b20af90c32426b2dd
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-11-04 16:05:46 -0300

    create projectList component and utils

---
+++ src/modules/projects/ProjectList/utils/reposToShow.ts
@@ -0,0 +1,12 @@
+export const repositoriesToShow = [
+  811007666, // RUTility
+  407364146, // Battleship-game-with-React
+  505688183, // Presearch-Bot-deprecated
+  398122061, // 4GeeksAcademy-RedSystem-Full-Stack-Final-Project-Backend-2021
+  395499242, // 4GeeksAcademy-RedSystem-Full-Stack-Final-Project-Frontend-2021
+];
+
+export const ignoreCaseForExcluding = [
+  398122061, // 4GeeksAcademy-RedSystem-Full-Stack-Final-Project-Backend-2021
+  395499242, // 4GeeksAcademy-RedSystem-Full-Stack-Final-Project-Frontend-2021
+];
