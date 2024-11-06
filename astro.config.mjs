023fb1da91bf140efd0dccc2c1ef2f0e458a6c3c
Pedro Yanez <72775296+wotanCode@users.noreply.github.com>
2024-11-06 15:26:49 -0300

    basic configuration for spanish language

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,13 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   integrations: [tailwind()],
   output: "hybrid",
   adapter: vercel(),
